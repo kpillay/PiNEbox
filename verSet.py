29f1dedf129ec413c0b396d6fc5cee94311b0be5
mcobo <maria.cobo@paediatrics.ox.ac.uk>
2021-04-14 21:46:50 +0100

    adding version info script

---
+++ verSet.py
@@ -0,0 +1,10 @@
+"""
+Class to set the version for PiNe app
+"""
+
+
+class verSet:
+    def __init__(self):
+        self.ver = '1.0 (BETA)'
+        self.releaseDate = '30.12.2020'
+        self.dev = 'Paediatric Neuroimaging'
