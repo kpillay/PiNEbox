8efdfdac30a4e2a7c1b92e668c17479922d77742
Kirubin Pillay <kirubin.pillay@paediatrics.ox.ac.uk>
2021-06-08 23:20:02 +0100

    button change test

--- PiNE_master.py
+++ PiNE_master.py
@@ -131,7 +131,7 @@ class PiNeMain(GUIaes):
         self.buttonFrame.grid(row=5, column=0, padx=70, pady=(40, 5), sticky=W)
 
         # Add RUN button
-        self.runImage = super().__renderImageOnly__(50, 50, 'start_icon_grey.png')
+        self.runImage = super().__renderImageOnly__(50, 50, 'unused/start_icon.png')
         self.runImageFaded = super().__renderImageOnly__(50, 50, 'start_icon_faded.png')
         self.runButton = Button(self.buttonFrame, image=self.runImage, relief=FLAT,
                                 compound=LEFT,  highlightthickness=0, borderwidth=0, highlightbackground='black',
